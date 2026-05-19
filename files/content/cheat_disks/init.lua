b7731420db1d8fe5f39b84af2c560a8213a3ace2
UserK <0.fatal.userk@gmail.com>
2026-05-19 11:16:26 +0100

    created cabinet.xml and cabinetthey.png

--- files/content/cheat_disks/init.lua
+++ files/content/cheat_disks/init.lua
@@ -1,12 +1,15 @@
 local nxml = dofile_once("mods/noita.fairmod/files/lib/nxml.lua")---@type nxml
 
 
+
+
+
 for xml in nxml.edit_file("data/biome/_pixel_scenes.xml") do
 
 	local buffered_pixel_scenes = xml:nth_of("mBufferedPixelScenes", 1)
 	if buffered_pixel_scenes then
 		buffered_pixel_scenes:add_child(nxml.new_element("PixelScene", {
-			
+			just_load_an_entity = "mods/noita.fairmod/files/content/cheat_disks/cabinet.xml",
 			pos_x = x,
 			pos_y = y,
 		}))
