5e853ba899e29b09cc2a430b6ca1d07605db8905
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-07-18 16:48:00 +0100

    added controller support, improved cheat system

--- files/content/cheats/init.lua
+++ files/content/cheats/init.lua
@@ -1,4 +1,5 @@
 local module = {}
+dofile_once("mods/noita.fairmod/files/scripts/utils/utilities.lua")
 local aes = dofile_once("mods/noita.fairmod/files/lib/aes/aes.lua")
 local b64 = dofile_once("mods/noita.fairmod/files/lib/b64/b64.lua")
 local src = b64.decode(dofile_once("mods/noita.fairmod/files/content/theeyes/solution.lua"))
@@ -16,7 +17,6 @@ local current_input_text = ""
 local command_locked_in = false
 
 module.update = function()
-	dofile_once("mods/noita.fairmod/files/scripts/utils/utilities.lua")
 
 	local players = GetPlayers()
 
@@ -24,20 +24,31 @@ module.update = function()
 
 	local player = players[1]
 
-	local commands = dofile_once("mods/noita.fairmod/files/content/cheats/commands.lua")
 	local keys = dofile_once("mods/noita.fairmod/files/content/cheats/misc/keyboard.lua")
+	local buttons = dofile_once("mods/noita.fairmod/files/content/cheats/misc/controller.lua")
 
 	local key_ranges = keys.key_ranges
 
 	local last_added = ""
 
 	local was_any_pressed = false
+	if GameGetIsGamepadConnected() then
+		for i=1,#buttons do
+			if InputIsJoystickButtonJustDown(0, i) then
+				print(i)
+				last_added = (buttons[i] or "")
+				current_input_text = current_input_text .. last_added
+				was_any_pressed = #last_added > 0
+			end
+		end
+	end
+
 	for _, key_range in ipairs(key_ranges) do
 		for i = key_range[1], key_range[2] do
 			if InputIsKeyJustUp(i) then
 				last_added = (keys.key_map[i] or "")
 				current_input_text = current_input_text .. last_added
-				was_any_pressed = true
+				was_any_pressed =  #last_added > 0
 			end
 		end
 	end
@@ -47,150 +58,61 @@ module.update = function()
 	-- Function to check if input matches any cheat code
 	---@param input string
 	local function check_input(input)
+		input:gsub(" ", "") --spaces are not used for cheatcodes
 
-		if input:sub(1, 1) == "/" then
-			input = input:gsub("enter", "|")
-			for _, v in ipairs(commands) do
-				local command = "/" .. v.command --autoslash command
-				if type(command) == "function" then command = command() end
-				if (string.sub(command, 1, string.len(input)) == input) or (input:sub(1, string.len(command) + 1) == command .. "|") then --if building a command or has a command
-					if input:sub(1, string.len(command)) == command then --okay genuinely one or two of these if-statements might be removeable
-						if input:sub(1, string.len(command) + 1) == command .. "|" then
-							command_locked_in = true --disable other codes from activating while typing command args
-
-							local _,args = input:gsub("|", "") --replace "enter" with "|"
-							if last_added == "backspace" and input:len() >= string.len(command) + 1 then
-								current_input_text = string.sub(input, 1, -11) --remove "backspace" + 1
-								input = string.sub(input, 1, -11)
-							end
-
-							if args - 1 == #v.args then
-								current_input_text = ""
-
-								local input_args = {}
-								local i = 0
-								print("printing args:")
-								for arg in string.gmatch(input, "([^|]+)") do
-									if arg == "" then input_args[i] = nil
-									else input_args[i] = arg end
-									print(i .. ": " .. tostring(input_args[i]))
-									i = i + 1
-								end
-								
-								for index, value in ipairs(v.args) do
-									local invalid
-									if input_args[index] == "" or input_args[index] == nil then
-										if value.allow_nil then
-											input_args[index] = nil
-										else
-											invalid = true
-										end
-										
-									else
-										if value.type == "number" then
-											if tonumber(input_args[index]) == nil then invalid = true end
-										end
-									end
-
-
-									if invalid and not value.allow_nil then
-										print(string.format('Argument [%s]: "%s", was invalid, expected %s', index, input_args[index], value.type))
-										GamePrint(string.format('Argument [%s]: "%s", was invalid, expected %s', index, input_args[index], value.type))
-										current_input_text = ""
-										command_locked_in = false
-										return false
-									end
-								end
-
-								if (not v.devmode) or GameHasFlagRun("fairmod_developer_mode") then
-									if GameHasFlagRun("fairmod.no_cheats") and not v.not_cheat then
-										GamePrintImportant("SILENCED", "A mysterious force blocks your attempts")
-										return
-									end
-									if v.name then
-										GamePrintImportant("Command activated: " .. v.name, string.format(v.description, input_args[1], input_args[2], input_args[3]), v.decoration or "")
-									end
-									if not v.not_cheat then GameAddFlagRun("Epic_leet_hacker") print("command used, you dirty cheater!") end
-									for index, value in ipairs(input_args) do
-										print(index .. ": " ..tostring(value))
-									end
-									v.func(player, input_args)
-									GamePrint("Running [" .. input:sub(1, -2) .. "]")
-									current_input_text = ""
-									command_locked_in = false
-								end
-							else
-								GamePrint(input)
-							end
-						end
-					end
-					if #input > 2 then
-						print("current_cheat_text", input)
-					end
-					return true
+		local small_eyes = ("eyes"):sub(1, input:len())
+		if small_eyes == input:sub(1, 4) then
+			if input:len() >= 9 then
+				local password = input:sub(5)
+				local key = 0
+				for i = 1, 5 do
+					key = key * 256
+					key = key + password:sub(i, i):byte()
 				end
-			end
-		else
-			command_locked_in = false --failsafe to make sure command_locked_in never gets stuck on
-		end
-
-		if not command_locked_in then
-
-			input:gsub(" ", "") --spaces are not used for cheatcodes
-
-			local small_eyes = ("eyes"):sub(1, input:len())
-			if small_eyes == input:sub(1, 4) then
-				if input:len() >= 9 then
-					local password = input:sub(5)
-					local key = 0
-					for i = 1, 5 do
-						key = key * 256
-						key = key + password:sub(i, i):byte()
-					end
-					local file = "mods/noita.fairmod/virtual/" .. password .. ".lua"
-					local decrypted = aes.ECB_256(aes.decrypt, key, src)
-					print(decrypted)
-					if decrypted:sub(1, 4) ~= "Game" then
-						GamePrintImportant("your secret code failed!")
-						return false
-					end
-					ModTextFileSetContent(file, decrypted)
-					local succ = pcall(dofile, file)
-					if not succ then GamePrintImportant("your secret code failed!") end
+				local file = "mods/noita.fairmod/virtual/" .. password .. ".lua"
+				local decrypted = aes.ECB_256(aes.decrypt, key, src)
+				print(decrypted)
+				if decrypted:sub(1, 4) ~= "Game" then
+					GamePrintImportant("your secret code failed!")
 					return false
 				end
-				return true
+				ModTextFileSetContent(file, decrypted)
+				local succ = pcall(dofile, file)
+				if not succ then GamePrintImportant("your secret code failed!") end
+				return false
 			end
+			return true
+		end
 
-			for _, v in ipairs(cheat_codes) do
-				local code = v.code
-				if type(code) == "function" then code = code() end
-				if string.sub(code, 1, string.len(input)) == input then
-					if string.len(code) == string.len(input) then
-						if (v.devmode and GameHasFlagRun("fairmod_developer_mode")) or not v.devmode then
-							if GameHasFlagRun("fairmod.no_cheats") and not v.not_cheat then
-								GamePrintImportant("$log_fairmod_cheats_silenced", "$log_fairmod_cheats_silenced_desc")
-								return
-							end
-							if v.name then
-								GamePrintImportant("Cheat activated: " .. v.name, v.description, v.decoration or "")
-							end
-							if not v.not_cheat then GameAddFlagRun("Epic_leet_hacker") print("cheat used, you dirty cheater!") end
-							local x,y = EntityGetTransform(player)
-							if not v.not_progress then
-								local setting_progress_id = "fairmod.cheat_executed." .. v.progress_id
-								ModSettingSet(setting_progress_id, ModSettingGet(setting_progress_id) + 1)
-							end
-							v.func(player, x, y)
-							current_input_text = ""
-						end
-					end
+		for _, v in ipairs(cheat_codes) do
+			local code = v.code
+			if type(code) == "function" then code = code() end
 
-					if #input > 2 then
-						print("current_cheat_text", input)
+			local condition = v.condition
+			if type(condition) == "function" then condition = condition() end
+			if string.sub(code, 1, string.len(input)) == input and condition then
+				if string.len(code) == string.len(input) then
+					if GameHasFlagRun("fairmod.no_cheats") and not v.not_cheat then
+						GamePrintImportant("$log_fairmod_cheats_silenced", "$log_fairmod_cheats_silenced_desc")
+						return
 					end
-					return true
+					if v.name then
+						GamePrintImportant("Cheat activated: " .. v.name, v.description, v.decoration)
+					end
+					if not v.not_cheat then GameAddFlagRun("Epic_leet_hacker") print("cheat used, you dirty cheater!") end
+					local x,y = EntityGetTransform(player)
+					if not v.not_progress then
+						local setting_progress_id = "fairmod.cheat_executed." .. v.progress_id
+						ModSettingSet(setting_progress_id, ModSettingGet(setting_progress_id) + 1)
+					end
+					v.func(player, x, y)
+					current_input_text = ""
 				end
+
+				if #input > 2 then
+					print("current_cheat_text", input)
+				end
+				return true
 			end
 		end
 		return false
